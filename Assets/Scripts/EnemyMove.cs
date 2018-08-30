09443d17f2b1f034c9306634e678684df92648fd
bk1729 <bhuvaneshkumar1729@gmail.com>
2018-08-30 17:45:13 +0530

    Initial commit

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour
+{
+	public float movementSpeed;
+	public bool canMove;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (this.canMove) {
+			this.transform.Translate (Vector3.left * this.movementSpeed * Time.deltaTime);
+		}
+	}
+}
